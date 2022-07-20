9218c5a2e4a8cdef8e3975829d68b00f9ae09576
Pramit Kumar Pal <pramitpalkumar@gmail.com>
2022-07-20 18:17:12 +0000

    modified:   level1_design1/test_mux.py  test commit

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,9 +6,12 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    # -----------
     module = dut
     inputs = [attr for attr in dir(module) if not callable(getattr(module, attr)) and attr.startswith("inp")]
     sel_lines = [attr for attr in dir(module) if not callable(getattr(module, attr)) and attr.startswith("sel")]
     outputs = [attr for attr in dir(module) if not callable(getattr(module, attr)) and attr.startswith("out")]
     print (outputs)   
+    # -----------------
+    
     cocotb.log.info('##### CTB: Develop your test here ########')
