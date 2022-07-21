29eeedce4ff0b147461e49d4caa2a5615b185b5a
Pramit Kumar Pal <pramitpalkumar@gmail.com>
2022-07-21 02:34:11 +0000

    try 1 succesful

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -11,7 +11,45 @@ async def test_mux(dut):
     inputs = [attr for attr in dir(module) if not callable(getattr(module, attr)) and attr.startswith("inp")]
     sel_lines = [attr for attr in dir(module) if not callable(getattr(module, attr)) and attr.startswith("sel")]
     outputs = [attr for attr in dir(module) if not callable(getattr(module, attr)) and attr.startswith("out")]
-    print (outputs)   
+    # print (type(inputs[0]))   
     # -----------------
-    
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.inp0.value=2
+    dut.inp1.value=2
+    dut.inp2.value=2
+    dut.inp3.value=2
+    dut.inp4.value=2
+    dut.inp5.value=2
+    dut.inp6.value=2
+    dut.inp7.value=2
+    dut.inp8.value=2
+    dut.inp9.value=2
+    dut.inp10.value=2
+    dut.inp11.value=2
+    dut.inp12.value=2
+    dut.inp13.value=2
+    dut.inp14.value=2
+    dut.inp15.value=2
+    dut.inp16.value=2
+    dut.inp17.value=2
+    dut.inp18.value=2
+    dut.inp19.value=2
+    dut.inp20.value=2
+    dut.inp21.value=2
+    dut.inp22.value=2
+    dut.inp23.value=2
+    dut.inp24.value=2
+    dut.inp25.value=2
+    dut.inp26.value=2
+    dut.inp27.value=2
+    dut.inp28.value=2
+    dut.inp29.value=2
+    dut.inp30.value=2
+    await Timer(2, units='ns')
+    i=28
+    dut.sel.value=i
+    await Timer(2, units='ns')
+    print(dut.out.value.integer)
+    # cocotb.log.info(dut.out.value == 2, "Output not match {OUT}".format(OUT=int(dut.out.value)))
+    assert dut.out.value == 2, "Output not match {OUT}".format(OUT=dut.out.value.integer)
+    # print("Output not match {OUT}".format(OUT=2))
+        
